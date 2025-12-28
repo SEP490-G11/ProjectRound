5e7f3a10756c04a9bc44dd05c6bb9e7b18041106
zzakonlovez <trungndhe170387@gmail.com>
2025-12-28 19:10:05 +0700

    Controller Test

---
+++ src/test/java/project/demo/controller/AuthControllerTest.java
@@ -0,0 +1,44 @@
+package project.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @Test
+    void registerRequestOtp() {
+    }
+
+    @Test
+    void registerVerifyOtp() {
+    }
+
+    @Test
+    void forgotRequestOtp() {
+    }
+
+    @Test
+    void forgotVerifyOtp() {
+    }
+
+    @Test
+    void forgotResetPassword() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void refresh() {
+    }
+
+    @Test
+    void logout() {
+    }
+}
